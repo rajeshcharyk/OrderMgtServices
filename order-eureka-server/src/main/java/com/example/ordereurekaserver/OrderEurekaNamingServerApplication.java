f2803819f661f40bf5784b9d05fdac001e34fa91
rajeshcharyk <k.rajeshchary@gmail.com>
2020-06-26 13:23:58 +0530

    Add files via upload
    Added Eureka Server configurations

---
+++ order-eureka-server/src/main/java/com/example/ordereurekaserver/OrderEurekaNamingServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.ordereurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class OrderEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEurekaNamingServerApplication.class, args);
+	}
+
+}
---
+++ order-eureka-server/src/test/java/com/example/ordereurekanamingserver/OrderEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ordereurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
